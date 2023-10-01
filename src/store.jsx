48bbbebbf022db8bf9b876fc3d05013d567f23c8
Fatma NABLI <nabli.isigk@gmail.com>
2023-10-01 17:53:04 +0200

    Updata the store

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,4 @@
 import { atom} from 'jotai'
 
 
-// export const myStore = createStore()
-
-// const countAtom = atom(0)
-// myStore.set(countAtom, 1)
-export const titreAtom = atom("Fatma");
 export const employeesAtom=atom(new Array())
-
-
